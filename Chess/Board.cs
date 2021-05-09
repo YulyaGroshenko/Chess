9ead54fdfbef2501604159ee0ffec054fd6541b0
Yulia Groshenko <yulyagroshenko@mail.ru>
2021-05-09 19:43:55 +0500

    доска

---
+++ Chess/Board.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Chess.Logic
+{
+    class Board
+    {
+        public Figure[,] Field { get; private set; }
+
+        public Board()
+        {
+            Field = new Figure[8, 8]   // Оy - цифры   Ох - буквы  
+            {
+                { new Rook(Sides.Black), new Knight(Sides.Black), new Bishop(Sides.Black), new King(Sides.Black), new Queen(Sides.Black), new Bishop(Sides.Black), new Knight(Sides.Black), new Rook(Sides.Black) },
+                { new Pawn(Sides.Black), new Pawn(Sides.Black), new Pawn(Sides.Black), new Pawn(Sides.Black), new Pawn(Sides.Black), new Pawn(Sides.Black), new Pawn(Sides.Black), new Pawn(Sides.Black) },
+                { null, null, null, null, null, null, null, null },
+                { null, null, null, null, null, null, null, null },
+                { null, null, null, null, null, null, null, null },
+                { null, null, null, null, null, null, null, null },
+                { new Pawn(Sides.White), new Pawn(Sides.White), new Pawn(Sides.White), new Pawn(Sides.White), new Pawn(Sides.White), new Pawn(Sides.White), new Pawn(Sides.White), new Pawn(Sides.White) },
+                { new Rook(Sides.White), new Knight(Sides.White), new Bishop(Sides.White), new King(Sides.White), new Queen(Sides.White), new Bishop(Sides.White), new Knight(Sides.White), new Rook(Sides.White) },
+            };
+        }
+        public void GetSaveField(Player player)
+        {
+            Field = player.Field;
+        }
+    }
+}
