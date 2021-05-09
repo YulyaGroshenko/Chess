f5744adb78e9f2aa157e0bc9451d4e56f1e783e8
Yulia Groshenko <yulyagroshenko@mail.ru>
2021-05-09 19:53:33 +0500

    игрок

---
+++ Chess/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess.Logic
+{
+    class Player
+    {
+        public string Name { get; private set; }
+        public int Victorys { get; private set; } 
+        public Figure[,] Field { get; private set; }
+        public Sides Side { get; private set; }
+        public Player(string name)
+        {
+            Name = name;
+            Side = Sides.White;
+        }
+        public void AssignSide(Sides side)
+        {
+            Side = side;
+        }
+        public void AddVictorys()
+        {
+            Victorys++;
+        }
+    }
+}
