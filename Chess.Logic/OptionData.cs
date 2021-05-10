567237f3c41b0c7197e91acea12ed190fdaff26d
Yulia Groshenko <yulyagroshenko@mail.ru>
2021-05-10 16:56:10 +0500

    реализация логики v0.1

---
+++ Chess.Logic/OptionData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chess.Logic
+{
+    static class OptionData
+    {
+        public static int FirstCellColor { get; set; }
+        public static int SecondCellColor { get; set; }
+    }
+}
--- Chess.Logic/Player.cs
+++ Chess.Logic/Player.cs
@@ -7,17 +7,13 @@ namespace Chess.Logic
     class Player
     {
         public string Name { get; private set; }
-        public int Victorys { get; private set; } 
+        public int Victorys { get; private set; } = 0;
         public Figure[,] Field { get; private set; }
-        public Sides Side { get; private set; }
+        public Sides Side { get; set; }
         public Player(string name)
         {
             Name = name;
             Side = Sides.White;
-        }
-        public void AssignSide(Sides side)
-        {
-            Side = side;
         }
         public void AddVictorys()
         {
