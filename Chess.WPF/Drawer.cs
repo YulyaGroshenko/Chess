7b7e993f4f0922993e95f47ef7d3babb6e83fd3b
Yulia Groshenko <yulyagroshenko@mail.ru>
2021-06-01 08:41:49 +0500

    меню + поле + обработка выбора клетки

---
+++ Chess.WPF/Drawer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using Chess.Logic;
+
+namespace Chess.WPF
+{
+    static class Drawer
+    {
+        public static void CreateField(Board board, Canvas canvas)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    Label label = new Label()
+                    {
+                        Content = board.Field[i, j].Abbreviation
+                    };
+                    Canvas.SetTop(label, i);
+                    Canvas.SetLeft(label, j);
+                    canvas.Children.Add(label);
+                }
+            }
+        }
+    }
+}
