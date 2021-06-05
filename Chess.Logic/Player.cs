281066918966f3af4633a63078790ee24cf95411
Yulia Groshenko <yulyagroshenko@mail.ru>
2021-06-05 20:33:46 +0500

    дебаг

--- Chess.Logic/Player.cs
+++ Chess.Logic/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Chess.Logic
@@ -7,17 +8,26 @@ namespace Chess.Logic
     public class Player
     {
         public string Name { get; private set; }
-        public int Victorys { get; private set; } = 0;
-        public Figure[,] Field { get; private set; }
+        public int Victorys { get; set; }
+        public Figure[,] Field { get; set; }
         public Sides Side { get; set; }
         public Player(string name)
         {
             Name = name;
             Side = Sides.White;
         }
-        public void AddVictorys()
-        {
-            Victorys++;
-        }
+        //public int AddVictorys()
+        //{
+        //    if (File.Exists($"{FileWorker.Saves}\\{Name}"))
+        //    {
+        //        Player player = FileWorker.GetPlayer(Name);
+        //        return player.Victorys++;
+        //    }
+        //    else
+        //    {
+        //        Victorys += 1;
+        //    }
+        //    return Victorys;
+        //}
     }
 }
